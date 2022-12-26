d02456ef006e7e4dcd42c8449286d6b2e40b6fb2
Anil Singh <anil927a@gmail.com>
2022-12-26 20:33:07 +0530

    assignment/counter-initialCommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './component/counter';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header> */}
       Hello World
+      <Counter/>
     </div>
   );
 }
---
+++ src/component/counter.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./counter.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0); // useState returns a pair. 'count' is the current state. 'setCount' is a function we can use to update the state.
+
+  function increment() {
+    //setCount(prevCount => prevCount+=1);
+    setCount(function (prevCount) {
+      return (prevCount += 1);
+    });
+  }
+
+  function decrement() {
+    setCount(function (prevCount) {
+      if (prevCount > 0) {
+        return (prevCount -= 1); 
+      } else {
+        return (prevCount = 0);
+      }
+    });
+  }
+
+  return (
+    <div className="Counter">
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
